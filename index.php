2341e80833d31434fc24d992fd138d2ce668ad1f
Takahashi <megane.nt5mf@gmail.com>
2022-03-15 12:21:08 +0900

    db connect test & show music list

--- index.php
+++ index.php
@@ -4,9 +4,34 @@
   <meta charset="utf-8">
   <title>りずしぇWEB</title>
 <body>
-  <h3>りずしぇうぇぶ</h3>
+  <h2>りずしぇうぇぶ</h2>
   <?php
-    echo 'hello world';
+    require './vendor/autoload.php';
+    Dotenv\Dotenv::createImmutable(__DIR__)->load();
+
+    try{
+        // db connect
+        $dsn = $_ENV['dsn'];
+        $username = $_ENV['username'];
+        $password = $_ENV['password'];
+        $dbh = new PDO($dsn, $username, $password);
+
+        $sth = $dbh -> prepare('select * from Music');
+        $sth -> execute();
+        $result = $sth -> fetchALL();
+
+        // print
+        echo "<h3>楽曲一覧</h3>";
+        echo "<ul>";
+        foreach($result as $row){
+            echo "<li><b>".$row['Title']."</b> by ".$row['Artist']."</li>";
+        }
+        echo "</ul>";
+
+    }catch (PDOException $e){
+        echo "<p>Error!". $e->getMessage()."</p>";
+    }
+
   ?>
 </body>
 </html>
